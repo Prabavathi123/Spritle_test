4a4e20b472d9fecda3e1486d6876ef7bbf55d510
Prabavathi123 <128578664+Prabavathi123@users.noreply.github.com>
2023-03-22 16:54:48 +0530

    Add files via upload

---
+++ expense-tracker-react-app/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
+import "./App.css";
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <AddTransaction />
+        <TransactionList />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
